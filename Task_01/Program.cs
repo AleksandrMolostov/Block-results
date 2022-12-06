07e7b7df486dceb73b8d2313cbb15737b2de31a9
Aleksandr <alexandr.igig@gmail.com>
2022-12-06 23:32:23 +0300

    Add algorithm

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -13,11 +13,14 @@ string[] array = FirstArray();
 string[] result = FindLessThan(array, 3);
 Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 
-string[] FindLessThan(string[] input, int n) {
+string[] FindLessThan(string[] input, int n)
+{
     string[] output = new string[CountLessThan(input, n)];
 
-    for(int i = 0, j = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
             output[j] = input[i];
             j++;
         }
@@ -26,11 +29,14 @@ string[] FindLessThan(string[] input, int n) {
     return output;
 }
 
-int CountLessThan(string[] input, int n) {
+int CountLessThan(string[] input, int n)
+{
     int count = 0;
 
-    for(int i = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
             count++;
         }
     }
@@ -38,7 +44,8 @@ int CountLessThan(string[] input, int n) {
     return count;
 }
 
-string[] FirstArray() {
+string[] FirstArray()
+{
     Console.Write("Введите значения через пробел: ");
     return Console.ReadLine().Split(" ");
 }
